808644c2100c5c0245065b9785af9f830003a73c
mreimundo <mreimundo@itaptec.com.ar>
2022-12-22 23:31:05 -0300

    Commit inicial

---
+++ backendCoderhouse/src/app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import cartsRoutes from './routes/cartsRoutes.js';
+import productsRoutes from './routes/productsRoutes.js';
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.listen(8080, () => console.log("Server OK! Listening PORT 8080"))
+app.use('/api/products/', productsRoutes)
+app.use('/api/carts/', cartsRoutes)
+app.use(express.static('public'))
