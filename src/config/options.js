b7c50bee2d547c2459b03184fada32fe9223ad79
mreimundo <mreimundo@itaptec.com.ar>
2023-02-05 11:22:43 -0300

    actualizo url de conexión

--- src/config/options.js
+++ src/config/options.js
@@ -3,7 +3,7 @@ const options = {
         productsFileName: "products.json"
     },
     mongoDb:{
-        url: "mongodb+srv://admin:admin@ecommerce.ua2jv9s.mongodb.net/test"
+        url: "mongodb+srv://admin:admin@ecommerce.ua2jv9s.mongodb.net/?retryWrites=true&w=majority"
     }
 }
 
